70562e661f8a7b0fcb9c5070e754f1d5e6ca72ee
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-08-07 18:04:46 -0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -11,3 +11,6 @@ this.changeY = function(y) {this.y += y;}
 }
 var objects = {};
 function createObject(src,x,y,w,h,name) {objects[name]=(new obj(src,x,y,w,h));}
+function render(objectData,canvas_query) {
+(new Canvas(canvas_query)).canvasJS.image(objectData.src,objectData.x,objectData.y,objectData.w,objectData.h);
+}
