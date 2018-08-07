443eae15be0848624a41073f391042e392bfe2e9
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-08-07 17:51:59 -0400

    Update newCanvas.js

--- newCanvas.js
+++ newCanvas.js
@@ -16,33 +16,33 @@ this.canvas = document.querySelectorAll(query)[0];
   if(f=="1") {
 this.canvasJS = {
   ctx: (new Canvas(query,"3")).ctx,
-rectangle: function(x,y,h,w){ctx.fillRect(x,y,h,w);},
+rectangle: function(x,y,h,w){this.ctx.fillRect(x,y,h,w);},
 text: {
-stroke:function(font,text,x,y){ctx.font = font;
-ctx.strokeText(text,x,y);},
-fill: function(font,text,x,y){ctx.font = font;
-ctx.fillText(text,x,y);}
+stroke:function(font,text,x,y){this.ctx.font = font;
+this.ctx.strokeText(text,x,y);},
+fill: function(font,text,x,y){this.ctx.font = font;
+this.ctx.fillText(text,x,y);}
 },
 gradientLinear: function(gx,gy,gx2,gy2,clr1,clr2,x,y,w,h){
-var grd = ctc.createLinearGradient(gx,gy,gx2,gy2);
+var grd = this.ctx.createLinearGradient(gx,gy,gx2,gy2);
 grd.addColorStop(0,clr1);
 grd.addColorStop(1,clr2);
 // Fill with gradient
-ctx.fillStyle = grd;
-ctx.fillRect(x,y,w,h);
+this.ctx.fillStyle = grd;
+this.ctx.fillRect(x,y,w,h);
 },
 gradientRadial: function(x1,y1,r1,x2,y2,r2,clr1,clr2,x,y,w,h){
-var grd = ctx.createRadialGradient(x1,y1,r1,x2,y2,r2);
+var grd = this.ctx.createRadialGradient(x1,y1,r1,x2,y2,r2);
 grd.addColorStop(0,clr1);
 grd.addColorStop(1,clr2);
 // Fill with gradient
-ctx.fillStyle = grd;
-ctx.fillRect(x,y,w,h);
+this.ctx.fillStyle = grd;
+this.ctx.fillRect(x,y,w,h);
 },
 image: function(source,x,y,w,h){
 imaged=new Image();
 imaged.src=source;
-ctx.drawImage(imaged,x,y,w,h);
+this.ctx.drawImage(imaged,x,y,w,h);
 }
 
 }};
